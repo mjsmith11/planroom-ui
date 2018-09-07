6907327e2a5cfc88715dc09f8ed7fe6404de2ce1
Matt Smith <mjsmith92@outlook.com>
2018-09-07 16:48:42 -0400

    Add and configure axios

--- src/main.js
+++ src/main.js
@@ -3,10 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vuelidate from 'vuelidate'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuelidate)
+
+axios.defaults.baseURL = process.env.VUE_APP_API_URL
 
 new Vue({
   router,
