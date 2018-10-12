9f976a7e2fb4acc8fc98a435532f10c5292798d1
Matt Smith <mjsmith92@outlook.com>
2018-10-12 17:25:46 -0400

    WIP events test.

--- tests/unit/plans/Lister.spec.js
+++ tests/unit/plans/Lister.spec.js
@@ -2,6 +2,7 @@ import { mount } from '@vue/test-utils'
 import Lister from '@/components/plans/Lister'
 import mockAxios from 'jest-mock-axios'
 import { EventBus } from '@/event-bus.js'
+import Vue from 'vue';
 
 describe('Job Index', () => {
   let cmp
@@ -17,6 +18,8 @@ describe('Job Index', () => {
   afterEach(() => {
     // cleaning up the mess left behind the previous test
     mockAxios.reset()
+    EventBus.$off('job-read')
+    EventBus.$off('file-uploaded')
   })
 
   it('filters object keys', () => {
@@ -39,5 +42,34 @@ describe('Job Index', () => {
     cmp.vm.loadList()
     mockAxios.mockResponse(response)
     expect(cmp.html()).toMatchSnapshot()
+  })
+
+  it('loads the list', () => {
+      const response = {
+          data: [
+              {
+                  key: '1/a blueprint.pdf',
+                  url: 'http://test.com/1/aBlueprint.com'
+              },
+              {
+                  key: '1/another blueprint.pdf',
+                  url: 'http://test.com/1/anotherBlueprint.com'
+              }
+          ]
+      }
+      cmp.vm.loadList()
+      mockAxios.mockResponse(response)
+      expect(cmp.vm.plans).toBe(response.data)
+  })
+
+  it('attaches events', () => {
+      const spy = jest.spyOn(cmp.vm, 'loadList')
+      cmp.vm.loadList();
+    //   console.log('attach begin')
+    //   EventBus.$emit('file-uploaded')
+    //   cmp.vm.$forceUpdate()
+    //   console.log('attach end')
+
+      expect(spy).toHaveBeenCalledTimes(1)
   })
 })
