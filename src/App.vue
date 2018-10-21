377c930128f2d63ba99abdf884c618303587f217
Matt Smith <mjsmith92@outlook.com>
2018-10-21 16:03:21 -0400

    Dispatch the logout action when we get 401

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,18 @@
     <router-view/>
   </div>
 </template>
+<script>
+import axios from 'axios'
+export default {
+  created () {
+    axios.interceptors.response.use(undefined, function (err) {
+      return new Promise(function (resolve, reject) {
+        if (err.status === 401 && err.config && !err.config.__isRetryRequest) {
+          this.$store.dispatch('logout')
+        }
+        throw err
+      })
+    })
+  }
+}
+</script>
