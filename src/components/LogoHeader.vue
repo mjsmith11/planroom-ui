728c36758675511af74d3ad553f8e6647a489c5f
Matt Smith <mjsmith92@outlook.com>
2018-10-30 22:26:24 -0400

    Adjust jumbotron padding

--- src/components/LogoHeader.vue
+++ src/components/LogoHeader.vue
@@ -58,6 +58,8 @@ h1 {
     margin-top: 10px;
     background-color: #f9f9f9;
     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    padding-top: 40px;
+    padding-bottom: 40px;
 }
 
 </style>
