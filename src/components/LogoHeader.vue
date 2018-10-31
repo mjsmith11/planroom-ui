8572f250f71ea5afdede5e83c9f8479b9af246ca
Matt Smith <mjsmith92@outlook.com>
2018-10-30 22:26:55 -0400

    Get rid of comments in logoheader

--- src/components/LogoHeader.vue
+++ src/components/LogoHeader.vue
@@ -1,28 +1,3 @@
-<!--<template>
-    <div class="header">
-        <img src="../assets/logo2.png" alt="Benchmark logo" class="src">
-        <div id="tagline"><strong>Planroom</strong></div>
-    </div>
-</template>
-<style scoped>
-img {
-    width: 100%;
-    display: block;
-    height: auto;
-}
-#tagline {
-    text-align: center;
-    margin-top: 20px;
-    margin-bottom: 20px;
-    background-color: #c9c9c9;
-}
-.header {
-    width: 80%;
-    margin: auto;
-    background-color: #fafafa;
-    padding: 2px 25px;
-}
-</style>-->
 <template>
     <div class="jumbotron">
         <div class="row">
