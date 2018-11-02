4bb67e776001aa03506a9f130dd9cdb194843036
vagrant <vagrant@vagrant.vm>
2018-11-02 17:29:15 +0000

    Test expired token view

---
+++ tests/unit/subcontractor/ExpiredToken.spec.js
@@ -0,0 +1,14 @@
+import { mount } from '@vue/test-utils'
+import ExpiredToken from '@/views/subcontractor/ExpiredToken'
+
+describe('Expired Token', () => {
+    it('has correct html structure', () => {
+        let cmp = mount(ExpiredToken, {
+            stubs: [
+                'logo-header',
+                'contact-info'
+            ]
+        })
+    expect(cmp.html()).toMatchSnapshot()
+    })
+})
