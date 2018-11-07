1660a799b6f8125ca7469c69eab6bc2c28b23d13
Matt Smith <mjsmith92@outlook.com>
2018-11-06 22:34:09 -0500

    Add tests for query parameter token

--- tests/unit/router.spec.js
+++ tests/unit/router.spec.js
@@ -4,6 +4,7 @@ import store from '@/store'
 describe('Router', () => {
   afterEach(() => {
     store.state.token = undefined
+    jest.clearAllMocks()
   })
   it('redirects to login when not logged in', () => {
     const to = {
@@ -57,5 +58,44 @@ describe('Router', () => {
     beforeLoginEnter(undefined, undefined, next)
 
     expect(next).toBeCalledWith('/jobs')
+  })
+  it('handles requests without query params', () => {
+    const next = jest.fn()
+
+    const to = {
+      matched: [{ meta: { requiresAuth: false } }]
+    }
+
+    store.dispatch = jest.fn()
+    beforeEach(to, undefined, next)
+    expect(store.dispatch).not.toBeCalled()
+  })
+  it('handles query params without token', () => {
+    const next = jest.fn()
+
+    const to = {
+      matched: [{ meta: { requiresAuth: false } }],
+      query: {
+        someField: "someValue"
+      }
+    }
+
+    store.dispatch = jest.fn()
+    beforeEach(to, undefined, next)
+    expect(store.dispatch).not.toBeCalled()
+  })
+  it('stores query param token in vuex', () => {
+    const next = jest.fn()
+
+    const to = {
+      matched: [{ meta: { requiresAuth: false } }],
+      query: {
+        token: "myToken"
+      }
+    }
+
+    store.dispatch = jest.fn()
+    beforeEach(to, undefined, next)
+    expect(store.dispatch).toBeCalledWith('subcontractorToken', 'myToken')
   })
 })
