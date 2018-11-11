a520eed7ff558035251d16ad1c7d903202126b49
Matt Smith <mjsmith92@outlook.com>
2018-11-11 17:31:06 -0500

    Test that it won't add invalid emails

--- tests/unit/jobs/Invite.spec.js
+++ tests/unit/jobs/Invite.spec.js
@@ -7,39 +7,46 @@ import mockAxios from 'jest-mock-axios'
 Vue.use(Vuelidate)
 
 describe('Add Job Form', () => {
-    let cmp
+  let cmp
 
-    const $route = {
-        params: {
-            id: 12
-        }
+  const $route = {
+    params: {
+      id: 12
     }
+  }
 
-    beforeEach(() => {
-        cmp = mount(Invite, {
-            mocks: {
-                $route
-            },
-            stubs: [
-                'font-awesome-icon'
-            ]
-        })
+  beforeEach(() => {
+    cmp = mount(Invite, {
+      mocks: {
+        $route
+      },
+      stubs: [
+        'font-awesome-icon'
+      ]
     })
+  })
 
-    afterEach(() => {
-        // cleaning up the mess left behind the previous test
-        mockAxios.reset()
-    })
+  afterEach(() => {
+    // cleaning up the mess left behind the previous test
+    mockAxios.reset()
+  })
 
-    it('has expected html structure', () => {
-        cmp.vm.formEmail = 'test1@email.com'
-        cmp.vm.$v.formEmail.$touch()
-        cmp.vm.addEmail()
+  it('has expected html structure', () => {
+    cmp.vm.formEmail = 'test1@email.com'
+    cmp.vm.$v.formEmail.$touch()
+    cmp.vm.addEmail()
 
-        cmp.vm.formEmail = 'test2@email.com'
-        cmp.vm.$v.formEmail.$touch()
-        cmp.vm.addEmail()
+    cmp.vm.formEmail = 'test2@email.com'
+    cmp.vm.$v.formEmail.$touch()
+    cmp.vm.addEmail()
 
-        expect(cmp.html()).toMatchSnapshot()
-    })
+    expect(cmp.html()).toMatchSnapshot()
+  })
+  it("doesn't add invalid email", () => {
+    cmp.vm.formEmail = 'notAnEmail'
+    cmp.vm.$v.formEmail.$touch()
+    cmp.vm.addEmail()
+
+    expect(cmp.vm.addresses.length).toBe(0)
+  })
 })
